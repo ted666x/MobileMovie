bda1a980dd4db0308e58ff04d0b3c27480cf5330
AnifPrabono <anif.10@students.amikom.ac.id>
2020-01-09 22:25:41 +0700

    add source code

---
+++ app/src/main/java/id/ac/amikom/myfilm/ahmadtedi/filemku/DataHelper.java
@@ -0,0 +1,32 @@
+package id.ac.amikom.myfilm.ahmadtedi.filemku;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by JamesAndrew on 12/31/2018.
+ */
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "filem.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table favorit(no INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, judul text null, tgl text null, ringkasan text null, image text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+
+    }
+}
